2427854d95a4590711efe2c1a220b094eaf7afb1
Rob Hitt <robhitt@gmail.com>
2017-02-05 10:50:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+componentWillReceiveProps(nextProps){
+  this.setState({
+    audienceIncreasing: nextProps.numAudienceMembers > this.props.numAudienceMembers,
+    audienceDecreasing: nextProps.numAudienceMembers < this.props.numAudienceMembers
+  })
+}
